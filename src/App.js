1074f33eb4210e5008a9654d4884277827264163
Adrian <adriantutuianu01@gmail.com>
2022-05-07 15:33:13 +0100

    Imported navbar.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.adriantut.dev";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times.</p>
-
-        <p>{10}</p>
-        <p>{"hello, ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        {/* Dynamic values as attribute values  */}
-        <a href={link}>Adrian site </a>
+        <h1>App Component</h1>
       </div>
     </div>
   );
