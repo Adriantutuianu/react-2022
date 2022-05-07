63b00b0061d8d25923485e24ba881b8039939944
Adrian <adriantutuianu01@gmail.com>
2022-05-07 16:16:02 +0100

    Passing an argument

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,21 @@ function Home() {
     console.log("hello");
   };
 
+  const handleClickAgain = (name) => {
+    console.log("hello" + name);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click Me!</button>
+      <button
+        onClick={() => {
+          handleClickAgain(" Mario");
+        }}
+      >
+        Click me again!
+      </button>
     </div>
   );
 }
