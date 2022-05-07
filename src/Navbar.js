a99eff2fdb31b9808eccda6027c45b3ad509930b
Adrian <adriantutuianu01@gmail.com>
2022-05-07 15:58:09 +0100

    Inline styling example

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,19 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>The Dojo blog</h1>
+      <h1>Adrian Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
