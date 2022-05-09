4f440eb5757295007ec09f4a3abdfe7c9e05f386
Adrian <adriantutuianu01@gmail.com>
2022-05-09 22:27:12 +0100

    Use state example- number

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 
 function Home() {
   const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
   // let name = "mario";
 
   const handleClick = () => {
     setName("luigi");
+    setAge(30);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
+      <p>
+        {name} is {age} years old.
+      </p>
       <button onClick={handleClick}>Click Me!</button>
     </div>
   );
