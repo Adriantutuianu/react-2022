042531820d1d49f588367711804ea112224717d1
Adrian <adriantutuianu01@gmail.com>
2022-05-18 10:20:46 +0100

    Route parameters

---
+++ src/BlogDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogDetails = () => {
+  return (
+    <div className="blog-details">
+      <h2>BlogDetails</h2>
+    </div>
+  );
+};
+
+export default BlogDetails;
